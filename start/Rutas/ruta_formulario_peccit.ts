6154c5d9273e40d72c807d555498fc9ba47a1263
Julio Jimenez <zaor.julio@gmail.com>
2023-08-29 15:46:42 -0500

    actualizacion encuestas

--- start/Rutas/ruta_formulario_peccit.ts
+++
@@ -1,6 +0,0 @@
-import Route from '@ioc:Adonis/Core/Route'
-const controlador = '../../../app/Presentacion/FormularioPeccit/ControladorFormulariosPeccit'
-
-Route.group(() => {
-  Route.get('/general', `${controlador}.visualizar`)
-}).prefix('/api/v1/formulario').middleware('autenticacionJwt')
