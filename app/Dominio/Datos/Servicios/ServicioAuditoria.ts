ebb338b28d1d8923e0a2055b0ae168f310f9f140
Julio Jimenez <zaor.julio@gmail.com>
2023-09-26 17:34:12 -0500

    Ajustes observación

--- app/Dominio/Datos/Servicios/ServicioAuditoria.ts
+++ app/Dominio/Datos/Servicios/ServicioAuditoria.ts
@@ -5,8 +5,6 @@ export class ServicioAuditoria{
 
   public async Auditar (datos: {}) {
       axios.post(`${Env.get('LOGS')}/auditoria`, datos).then(resp =>{
-        console.log('guardo log');
-        
       }).catch(err =>{
         console.log("Fallo auditoria", err);
         
