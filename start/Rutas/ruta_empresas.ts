4b62db9fc67d1a5b4e16d6aa4f7c867172be388a
jorgemdiazp <jorgemdiazp@gmail.com>
2024-10-13 19:23:03 -0500

    amparos

--- start/Rutas/ruta_empresas.ts
+++ start/Rutas/ruta_empresas.ts
@@ -5,5 +5,5 @@ Route.group(() => {
   Route.get('/listarempresas', accion_path + '.listar')
   Route.get('/listarpoliza', accion_path + '.listarpoliza')
   Route.get('/novedades_poliza', accion_path + '.novedadespoliza')
-
+  Route.get('/amparos_poliza', accion_path + '.amparospoliza')
 }).prefix('api/v1/empresas').middleware('autenticacionJwt')
